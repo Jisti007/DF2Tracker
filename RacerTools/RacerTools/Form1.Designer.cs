7c0745dcc7a1b08ef8e40d06ef8e656afcae289b
Jaakko Tammela <tammi_574@hotmail.com>
2016-10-18 09:24:16 +0300

    init commit

---
+++ RacerTools/RacerTools/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace RacerTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.GameAvailableTimer = new System.Windows.Forms.Timer(this.components);
+            this.savePositionbtn = new System.Windows.Forms.Button();
+            this.gotoPositionbtn = new System.Windows.Forms.Button();
+            this.gotoZerobtn = new System.Windows.Forms.Button();
+            this.positionTimer = new System.Windows.Forms.Timer(this.components);
+            this.curPosXLbl = new System.Windows.Forms.Label();
+            this.curPosZLbl = new System.Windows.Forms.Label();
+            this.curPosYLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addSavedYValue = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(13, 13);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(183, 13);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Status: Looking for SWEP1RCR.exe ";
+            // 
+            // GameAvailableTimer
+            // 
+            this.GameAvailableTimer.Enabled = true;
+            this.GameAvailableTimer.Tick += new System.EventHandler(this.GameAvailableTimer_Tick);
+            // 
+            // savePositionbtn
+            // 
+            this.savePositionbtn.Location = new System.Drawing.Point(27, 64);
+            this.savePositionbtn.Name = "savePositionbtn";
+            this.savePositionbtn.Size = new System.Drawing.Size(76, 45);
+            this.savePositionbtn.TabIndex = 4;
+            this.savePositionbtn.Text = "Save current position";
+            this.savePositionbtn.UseVisualStyleBackColor = true;
+            this.savePositionbtn.Click += new System.EventHandler(this.savePositionbtn_Click);
+            // 
+            // gotoPositionbtn
+            // 
+            this.gotoPositionbtn.Location = new System.Drawing.Point(142, 64);
+            this.gotoPositionbtn.Name = "gotoPositionbtn";
+            this.gotoPositionbtn.Size = new System.Drawing.Size(83, 45);
+            this.gotoPositionbtn.TabIndex = 5;
+            this.gotoPositionbtn.Text = "Go to saved position";
+            this.gotoPositionbtn.UseVisualStyleBackColor = true;
+            this.gotoPositionbtn.Click += new System.EventHandler(this.gotoPositionbtn_Click);
+            // 
+            // gotoZerobtn
+            // 
+            this.gotoZerobtn.Location = new System.Drawing.Point(182, 228);
+            this.gotoZerobtn.Name = "gotoZerobtn";
+            this.gotoZerobtn.Size = new System.Drawing.Size(90, 22);
+            this.gotoZerobtn.TabIndex = 6;
+            this.gotoZerobtn.Text = "Go to 0, 0, 0";
+            this.gotoZerobtn.UseVisualStyleBackColor = true;
+            this.gotoZerobtn.Click += new System.EventHandler(this.gotoZerobtn_Click);
+            // 
+            // positionTimer
+            // 
+            this.positionTimer.Enabled = true;
+            this.positionTimer.Tick += new System.EventHandler(this.positionTimer_Tick);
+            // 
+            // curPosXLbl
+            // 
+            this.curPosXLbl.AutoSize = true;
+            this.curPosXLbl.Location = new System.Drawing.Point(24, 163);
+            this.curPosXLbl.Name = "curPosXLbl";
+            this.curPosXLbl.Size = new System.Drawing.Size(48, 13);
+            this.curPosXLbl.TabIndex = 7;
+            this.curPosXLbl.Text = "X: empty";
+            // 
+            // curPosZLbl
+            // 
+            this.curPosZLbl.AutoSize = true;
+            this.curPosZLbl.Location = new System.Drawing.Point(24, 189);
+            this.curPosZLbl.Name = "curPosZLbl";
+            this.curPosZLbl.Size = new System.Drawing.Size(48, 13);
+            this.curPosZLbl.TabIndex = 8;
+            this.curPosZLbl.Text = "Z: empty";
+            // 
+            // curPosYLbl
+            // 
+            this.curPosYLbl.AutoSize = true;
+            this.curPosYLbl.Location = new System.Drawing.Point(24, 216);
+            this.curPosYLbl.Name = "curPosYLbl";
+            this.curPosYLbl.Size = new System.Drawing.Size(48, 13);
+            this.curPosYLbl.TabIndex = 9;
+            this.curPosYLbl.Text = "Y: empty";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Saved position";
+            // 
+            // addSavedYValue
+            // 
+            this.addSavedYValue.Location = new System.Drawing.Point(117, 211);
+            this.addSavedYValue.Name = "addSavedYValue";
+            this.addSavedYValue.Size = new System.Drawing.Size(26, 22);
+            this.addSavedYValue.TabIndex = 11;
+            this.addSavedYValue.Text = "+";
+            this.addSavedYValue.UseVisualStyleBackColor = true;
+            this.addSavedYValue.Click += new System.EventHandler(this.addSavedYValue_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.addSavedYValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.curPosYLbl);
+            this.Controls.Add(this.curPosZLbl);
+            this.Controls.Add(this.curPosXLbl);
+            this.Controls.Add(this.gotoZerobtn);
+            this.Controls.Add(this.gotoPositionbtn);
+            this.Controls.Add(this.savePositionbtn);
+            this.Controls.Add(this.statusLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Timer GameAvailableTimer;
+        private System.Windows.Forms.Button gotoPositionbtn;
+        private System.Windows.Forms.Button savePositionbtn;
+        private System.Windows.Forms.Button gotoZerobtn;
+        private System.Windows.Forms.Timer positionTimer;
+        private System.Windows.Forms.Label curPosXLbl;
+        private System.Windows.Forms.Label curPosZLbl;
+        private System.Windows.Forms.Label curPosYLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addSavedYValue;
+    }
+}
+
---
+++ RacerTools/RacerTools/Form1.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Globalization;
+using System.Threading;
+
+//made using tutorial by Guided Hacker https://www.youtube.com/watch?v=0osZuafJuB0
+//KeyboardHook and Memory not made by me, they are made by the guys credited in the files
+
+namespace RacerTools
+{
+    public partial class Form1 : Form
+    {
+        #region Global variables
+        Memory myMemory = new Memory();
+        Process[] myProcess;
+        bool isGameAvailable = false;
+        
+        string xPointer = "004BFD28";
+        int[] xOff = {0x50};
+        string zPointer = "004D7878";
+        int[] zOff = {0x54};
+        string yPointer = "00E29C04";
+        int[] yOff = {0x58};
+
+        float xCoord = 0;
+        float zCoord = 0;
+        float yCoord = 0;
+
+        float savedXcoord;
+        float savedZcoord;
+        float savedYcoord;
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KeyboardHook.CreateHook(KeyReader);
+        }
+
+        private void gotoZerobtn_Click(object sender, EventArgs e)
+        {
+            goToZeroPosition();
+        }
+        private void savePositionbtn_Click(object sender, EventArgs e)
+        {
+            #region X
+            if (isGameAvailable)
+            {
+                myMemory.ReadProcess = myProcess[0];
+                myMemory.Open();
+                int XpointerAddress = HexToDec(xPointer);
+                int[] XpointerOffSet = xOff;
+                int bytesRead;
+                uint valueToRead = 50;
+                byte[] readAddress = myMemory.PointerRead((IntPtr)XpointerAddress, valueToRead, XpointerOffSet, out bytesRead);
+                savedXcoord = BitConverter.ToSingle(readAddress, 0);
+                myMemory.CloseHandle();
+            }
+            #endregion
+            #region Z
+            if (isGameAvailable)
+            {
+                myMemory.ReadProcess = myProcess[0];
+                myMemory.Open();
+                int ZpointerAddress = HexToDec(zPointer);
+                int[] ZpointerOffSet = zOff;
+                int bytesRead;
+                uint valueToRead = 54;
+                byte[] readAddress = myMemory.PointerRead((IntPtr)ZpointerAddress, valueToRead, ZpointerOffSet, out bytesRead);
+                savedZcoord = BitConverter.ToSingle(readAddress, 0);
+                myMemory.CloseHandle();
+            }
+            #endregion
+            #region Y
+            if (isGameAvailable)
+            {
+                myMemory.ReadProcess = myProcess[0];
+                myMemory.Open();
+                int YpointerAddress = HexToDec(yPointer);
+                int[] YpointerOffSet = yOff;
+                int bytesRead;
+                uint valueToRead = 54;
+                byte[] readAddress = myMemory.PointerRead((IntPtr)YpointerAddress, valueToRead, YpointerOffSet, out bytesRead);
+                savedYcoord = BitConverter.ToSingle(readAddress, 0);
+                myMemory.CloseHandle();
+            }
+            #endregion
+        }
+        private void gotoPositionbtn_Click(object sender, EventArgs e)
+        {
+            #region X
+            if (isGameAvailable)
+            {
+                myMemory.ReadProcess = myProcess[0];
+                myMemory.Open();
+                int XpointerAddress = HexToDec(xPointer);
+                int[] XpointerOffSet = xOff;
+                int bytesWritten;
+                byte[] valueToWrite = BitConverter.GetBytes(savedXcoord);
+                string writtenAddress = myMemory.PointerWrite((IntPtr)XpointerAddress, valueToWrite, XpointerOffSet, out bytesWritten);
+                myMemory.CloseHandle();
+            }
+            #endregion
+            #region Z
+            if (isGameAvailable)
+            {
+                myMemory.ReadProcess = myProcess[0];
+                myMemory.Open();
+                int ZpointerAddress = HexToDec(zPointer);
+                int[] ZpointerOffSet = zOff;
+                int bytesWritten;
+                byte[] valueToWrite = BitConverter.GetBytes(savedZcoord);
+                string writtenAddress = myMemory.PointerWrite((IntPtr)ZpointerAddress, valueToWrite, ZpointerOffSet, out bytesWritten);
+                myMemory.CloseHandle();
+            }
+            #endregion
+            #region Y
+            if (isGameAvailable)
+            {
+                myMemory.ReadProcess = myProcess[0];
+                myMemory.Open();
+                int YpointerAddress = HexToDec(yPointer);
+                int[] YpointerOffSet = yOff;
+                int bytesWritten;
+                byte[] valueToWrite = BitConverter.GetBytes(savedYcoord);
+                string writtenAddress = myMemory.PointerWrite((IntPtr)YpointerAddress, valueToWrite, YpointerOffSet, out bytesWritten);
+                myMemory.CloseHandle();
+            }
+            #endregion
+        }
+        private void GameAvailableTimer_Tick(object sender, EventArgs e)
+        {
+            myProcess = Process.GetProcessesByName("SWEP1RCR");
+            if (myProcess.Length != 0)
+            {
+                isGameAvailable = true;
+                statusLabel.Text = "Status: SWEP1RCR.exe found";
+            }
+            else
+            {
+                statusLabel.Text = "Status: SWEP1RCR.exe NOT found";
+            }
+        }
+        public static string HexToDec(int DEC)
+        {
+            return DEC.ToString("X");
+        }
+        public static int HexToDec(string Hex)
+        {
+            return int.Parse(Hex, NumberStyles.HexNumber);
+        }
+
+        public void KeyReader(IntPtr wParam, IntPtr lParam)
+        {
+            int key = Marshal.ReadInt32(lParam);
+            KeyboardHook.VK vk = (KeyboardHook.VK)key;
+            String temp = "";
+
+            switch (vk)
+            {
+                case KeyboardHook.VK.VK_F1:
+                    temp = "<-F1->";
+                    break;
+                case KeyboardHook.VK.VK_F2:
+                    temp = "<-F2->";
+                    break;
+                case KeyboardHook.VK.VK_F3:
+                    temp = "<-F3->";
+                    break;
+                case KeyboardHook.VK.VK_F4:
+                    temp = "<-F4->";
+                    break;
+                case KeyboardHook.VK.VK_F5:
+                    temp = "<-F5->";
+                    break;
+                case KeyboardHook.VK.VK_F6:
+                    temp = "<-F6->";
+                    break;
+                case KeyboardHook.VK.VK_F7:
+                    temp = "<-F7->";
+                    break;
+                case KeyboardHook.VK.VK_F8:
+                    temp = "<-F8->";
+                    break;
+                case KeyboardHook.VK.VK_F9:
+                    temp = "<-F9->";
+                    break;
+                case KeyboardHook.VK.VK_F10:
+                    temp = "<-F10->";
+                    break;
+                case KeyboardHook.VK.VK_F11:
+                    temp = "<-F11->";
+                    break;
+                case KeyboardHook.VK.VK_F12:
+                    temp = "<-F12->";
+                    break;
+                case KeyboardHook.VK.VK_LSHIFT:
+                    temp = "<-left shift->";
+                    break;
+                case KeyboardHook.VK.VK_LCONTROL:
+                    temp = "<-left control->";
+                    break;
+                case KeyboardHook.VK.VK_SUBTRACT:
+                    temp = "-";
+                    break;
+                case KeyboardHook.VK.VK_ADD:
+                    temp = "+";
+                    break;
+                case KeyboardHook.VK.VK_DECIMAL:
+                    temp = ".";
+                    break;
+                case KeyboardHook.VK.VK_DIVIDE:
+                    temp = "/";
+                    break;
+                case KeyboardHook.VK.VK_NUMPAD0:
+                    temp = "0";
+                    break;
+                case KeyboardHook.VK.VK_NUMPAD1:
+                    temp = "1";
+                    break;
+                case KeyboardHook.VK.VK_NUMPAD2:
+                    temp = "2";
+                    break;
+                case KeyboardHook.VK.VK_NUMPAD3:
+                    temp = "3";
+                    break;
+                case KeyboardHook.VK.VK_NUMPAD4:
+                    temp = "4";
+                    break;
+                case KeyboardHook.VK.VK_NUMPAD5:
+                    temp = "5";
+                    break;
+                case KeyboardHook.VK.VK_NUMPAD6:
+                    temp = "6";
+                    break;
+                case KeyboardHook.VK.VK_NUMPAD7:
+                    temp = "7";
+                    break;
+                case KeyboardHook.VK.VK_NUMPAD8:
+                    temp = "8";
+                    break;
+                case KeyboardHook.VK.VK_NUMPAD9:
+                    temp = "9";
+                    break;
+                case KeyboardHook.VK.VK_0:
+                    temp = "0";
+                    break;
+                case KeyboardHook.VK.VK_1:
+                    temp = "1";
+                    break;
+                case KeyboardHook.VK.VK_2:
+                    temp = "2";
+                    break;
+                case KeyboardHook.VK.VK_3:
+                    temp = "3";
+                    break;
+                case KeyboardHook.VK.VK_4:
+                    temp = "4";
+                    break;
+                case KeyboardHook.VK.VK_5:
+                    temp = "5";
+                    break;
+                case KeyboardHook.VK.VK_6:
+                    temp = "6";
+                    break;
+                case KeyboardHook.VK.VK_7:
+                    temp = "7";
+                    break;
+                case KeyboardHook.VK.VK_8:
+                    temp = "8";
+                    break;
+                case KeyboardHook.VK.VK_9:
+                    temp = "9";
+                    break;
+                default: break;
+            }
+
+            #region Key triggers
+            if (temp == "1")
+            {
+                if (isGameAvailable)
+                {
+                    goToZeroPosition();
+                }
+            }
+            #endregion
+        }
+
+        public void goToZeroPosition()
+        {
+            #region X
+            if (isGameAvailable)
+            {
+                myMemory.ReadProcess = myProcess[0];
+                myMemory.Open();
+                int XpointerAddress = HexToDec(xPointer);
+                int[] XpointerOffSet = xOff;
+                int bytesWritten;
+                byte[] valueToWrite = BitConverter.GetBytes(xCoord);
+                string writtenAddress = myMemory.PointerWrite((IntPtr)XpointerAddress, valueToWrite, XpointerOffSet, out bytesWritten);
+                myMemory.CloseHandle();
+            }
+            #endregion
+            #region Z
+            if (isGameAvailable)
+            {
+                myMemory.ReadProcess = myProcess[0];
+                myMemory.Open();
+                int ZpointerAddress = HexToDec(zPointer);
+                int[] ZpointerOffSet = zOff;
+                int bytesWritten;
+                byte[] valueToWrite = BitConverter.GetBytes(zCoord);
+                string writtenAddress = myMemory.PointerWrite((IntPtr)ZpointerAddress, valueToWrite, ZpointerOffSet, out bytesWritten);
+                myMemory.CloseHandle();
+            }
+            #endregion
+            #region Y
+            if (isGameAvailable)
+            {
+                myMemory.ReadProcess = myProcess[0];
+                myMemory.Open();
+                int YpointerAddress = HexToDec(yPointer);
+                int[] YpointerOffSet = yOff;
+                int bytesWritten;
+                byte[] valueToWrite = BitConverter.GetBytes(yCoord);
+                string writtenAddress = myMemory.PointerWrite((IntPtr)YpointerAddress, valueToWrite, YpointerOffSet, out bytesWritten);
+                myMemory.CloseHandle();
+            }
+            #endregion
+        }
+
+        private void positionTimer_Tick(object sender, EventArgs e)
+        {
+            curPosXLbl.Text = "X: " + savedXcoord;
+            curPosYLbl.Text = "Y: " + savedYcoord;
+            curPosZLbl.Text = "Z: " + savedZcoord;
+        }
+
+        private void addSavedYValue_Click(object sender, EventArgs e)
+        {
+            savedYcoord = savedYcoord + 100;
+        }
+    }
+
+}
